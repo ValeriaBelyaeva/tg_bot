ea8544b7686a170e97f1ebf35e491485caed8227
ValeriaBelyaeva <lera.belyayeva.2006@mail.ru>
2024-09-22 13:12:52 +0700

    translated comments into English

--- data.py
+++ data.py
@@ -1,8 +1,3 @@
-# file with data
-
-
-
-import requests
 from aiogram import Bot, Dispatcher
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
@@ -11,13 +6,12 @@ from constants import BOT_TOKEN
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-cat_response: requests.Response
 cat_link: str
 offset = -2
 
-# Создаем объекты кнопок
+# Creating button objects
 button_1 = KeyboardButton(text='Хочу котика (не 🦮)')
 button_2 = KeyboardButton(text='я огурец 🥒')
 
-# Создаем объект клавиатуры, добавляя в него кнопки
+# Create a keyboard object by adding buttons to it
 keyboard = ReplyKeyboardMarkup(keyboard=[[button_1, button_2]])
